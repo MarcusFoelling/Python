9a30290cf7570ee50a07f0ebc98e8612cdbe6fd0
Marcus Andreas Fölling <marcus@foelling.net>
2025-08-03 13:57:46 +0200

    Geburtstag

---
+++ Geburtstag.py
@@ -0,0 +1,22 @@
+import locale
+from datetime import datetime
+
+locale.setlocale(locale.LC_TIME, "de_DE.UTF-8")
+
+while True:
+    eingabe = input("Wann wurdest Du geboren? (TT.MM.JJJJ) ")
+    try:
+        geburtstag = datetime.strptime(eingabe, "%d.%m.%Y")
+        break
+    except ValueError:
+        print("Eingabe fehlerhaft, bitte erneut versuchen.")
+
+wochentag = geburtstag.strftime("%A")
+print(f"Du wurdest an einem {wochentag} geboren.")
+
+differenz = datetime.today() - geburtstag
+print(f"Du bist bereits {differenz.total_seconds()} Sekunden alt")
+print(f"oder {differenz.total_seconds() / 60} Minuten")
+print(f"oder {differenz.total_seconds() / 60 / 60} Stunden")
+print(f"oder {differenz.total_seconds() / 60 / 60 / 24} Tage")
+print(f"oder {differenz.total_seconds() / 60 / 60 / 24 / 365} Jahre")
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Python
 
 Verschiedene Python-Projekte, um insbesondere (junge) Anfänger*innen an die Programmiersprache heranzuführen.
+
+weitere Details unter [www.foelling.net/tags/python](www.foelling.net/tags/python)
